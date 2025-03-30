0c228829545c3ed5f5106d8102fc2a0d1ebd4f3f
Rameshwor <r.sresta07@gmail.com>
2025-03-30 13:54:24 +0545

    [Add]: All Blog page

--- src/components/common/CommonHeader.tsx
+++ src/components/common/CommonHeader.tsx
@@ -1,20 +1,22 @@
 import Link from "next/link";
-import headerData from "@/utils/mock/headerData.json"
-import CommonLogo from "@/components/common/CommonLogo"
+import headerData from "@/utils/mock/headerData.json";
+import CommonLogo from "@/components/common/CommonLogo";
 
 export default function CommonHeader() {
   return (
-    <main className="flex justify-between items-center container mx-auto">
-      <CommonLogo/>
-      <section className='pr-12 pt-4'>
-        <ul className="flex gap-12 text-darkFontColor">
+    <main className="flex justify-between items-center container mx-auto pt-4">
+      <CommonLogo />
+      <section className="">
+        <ul className="flex items-center gap-12 text-darkFontColor">
           {headerData?.options?.map((item: any) => (
             <li key={item.id}>
-              <Link href={item?.link} className={`text-xl`}>{item?.title}</Link>
+              <Link href={item?.link} className={`text-xl`}>
+                {item?.title}
+              </Link>
             </li>
           ))}
         </ul>
       </section>
     </main>
-  )
+  );
 }
--- src/components/common/CommonLogo.tsx
+++ src/components/common/CommonLogo.tsx
@@ -1,19 +1,26 @@
 import Image from "next/image";
-import headerData from "@/utils/mock/headerData.json"
-import {useRouter} from "next/router";
+import headerData from "@/utils/mock/headerData.json";
+import { useRouter } from "next/router";
+import commonData from "@/utils/mock/commonData.json";
+import Link from "next/link";
 
 const CommonLogo = () => {
   const router = useRouter();
   return (
-    <Image
-      src={headerData?.logoImage}
-      alt={headerData?.logoTitle}
-      width={1024}
-      height={1024}
-      onClick={() => router.push("/")}
-      className="h-[2rem] w-auto object-contain"
-    />
-  )
-}
+    <Link href="/">
+      {/* <Image
+        src={headerData?.logoImage}
+        alt={headerData?.logoTitle}
+        width={1024}
+        height={1024}
+        onClick={() => router.push("/")}
+        className="h-[2rem] w-auto object-contain"
+      /> */}
+      <h1 className="text-center text-4xl font-bold text-darkFontColor">
+        {commonData?.projectTitle}
+      </h1>
+    </Link>
+  );
+};
 
 export default CommonLogo;
