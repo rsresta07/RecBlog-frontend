cb3afeb39c6e6f536e6943dfa087fbb9a4fc6cb0
Rameshwor <r.sresta07@gmail.com>
2025-04-30 13:45:52 +0545

    [fixes]: load page with slug

--- src/pages/blog/[postSlug].tsx
+++ src/pages/blog/[postSlug].tsx
@@ -1,20 +1,54 @@
 import HeroLayout from "@/layouts/HeroLayout";
 import { useRouter } from "next/router";
-import posts from "@/utils/mock/posts.json";
 import Image from "next/image";
 import Link from "next/link";
+import { ApiGetPost, APIGetPostDetails } from "@/api/blog";
+import { useEffect, useState } from "react";
 
 const PostDetail = () => {
   const router = useRouter();
-  const postSlug = router.query;
+  const postSlug = router.query.postSlug;
+  const [details, setDetails] = useState<any>();
+  const [postData, setPostData] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchBlogDetails = async () => {
+    try {
+      const response = await APIGetPostDetails(postSlug);
+      const data = response?.data;
+      setDetails(data);
+    } catch (error) {
+      console.error("Failed to fetch:", error);
+    }
+  };
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await ApiGetPost();
+      setPostData(response?.data);
+      console.log("Fetching post data:", response?.data);
+    } catch (error) {
+      console.error("Failed to fetch:", error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+    if (postSlug) {
+      fetchBlogDetails();
+    }
+  }, [postSlug]);
   return (
     <main className="container mx-auto grid grid-cols-10 gap-[4rem] mt-[4rem]">
+      {/*Recent Blogs*/}
       <section className="col-span-3">
         <h2 className="text-2xl font-bold text-darkFontColor mb-4">
           Recent blog post
         </h2>
         <div>
-          {posts?.slice(0, 6)?.map((post) => (
+          {postData?.slice(0, 6)?.map((post) => (
             <div className={`mb-8`} key={post?.id}>
               <Link href={`/blog/${post?.slug}`}>
                 <Image
@@ -27,16 +61,22 @@ const PostDetail = () => {
               </Link>
               <div>
                 <span className={`text-purple-700 text-sm`}>
-                  <Link href={`#`}>{post?.author}</Link>&nbsp;-&nbsp;
-                  <Link href={`#`}>{post?.date}</Link>
+                  {post?.users?.[0] && (
+                    <Link href={`/user/${post?.users[0]?.slug}`}>
+                      {post?.users[0]?.fullName}
+                    </Link>
+                  )}
+                  {/*&nbsp;-&nbsp;*/}
+                  {/*<Link href={`#`}>{post?.date}</Link>*/}
                 </span>
                 <Link href={`/blog/${post?.slug}`}>
                   <h3 className={`text-xl line-clamp-1`}>{post?.title}</h3>
-                  <p className={`mb-4 line-clamp-3 text-sm`}>
-                    {post?.description}
-                  </p>
+                  <p
+                    dangerouslySetInnerHTML={{ __html: post?.content }}
+                    className={`mb-4 line-clamp-3 text-sm`}
+                  />
                 </Link>
-                {post?.tag?.map((tag) => (
+                {post?.tags?.map((tag: any) => (
                   <span
                     key={tag?.id}
                     className="text-sm px-2 bg-purple-200 rounded-lg text-purple-700 m-1"
@@ -50,65 +90,34 @@ const PostDetail = () => {
         </div>
       </section>
       <section className="col-span-7 flex flex-col gap-8">
-        <span className={`text-purple-700 text-sm`}>
-          <Link href={`#`}>Rameshwor</Link>&nbsp;-&nbsp;
-          <Link href={`#`}>2025/03/30</Link>
+        <span className={`text-purple-700`}>
+          {details?.users?.[0] && (
+            <Link href={`/user/${details?.users[0]?.slug}`}>
+              {details?.users[0]?.fullName}
+            </Link>
+          )}
+          {/*&nbsp;-&nbsp;*/}
+          {/*<Link href={`#`}>2025/03/30</Link>*/}
         </span>
-        <h2 className={`text-4xl font-bold`}>UX review presentation</h2>
+        <h2 className={`text-4xl font-bold`}>{details?.title}</h2>
+        <div>
+          {details?.tags?.map((tag: any) => (
+            <span
+              key={tag?.id}
+              className="px-2 bg-purple-200 rounded-lg text-purple-700 m-1"
+            >
+              <Link href="#">{tag?.title}</Link>
+            </span>
+          ))}
+        </div>
         <Image
-          src="/L007.jpg"
-          alt="blog post"
+          src={details?.image}
+          alt={details?.title}
           width={1024}
           height={1024}
           className="w-auto object-cover"
         />
-        <p>
-          How do you create compelling presentations that wow your colleagues
-          and impress your managers?
-        </p>
-        <p>
-          A grid system is a design tool used to arrange content on a webpage.
-          It is a series of vertical and horizontal lines that create a matrix
-          of intersecting points, which can be used to align and organize page
-          elements. Grid systems are used to create a consistent look and feel
-          across a website, and can help to make the layout more visually
-          appealing and easier to navigate.
-        </p>
-        <p>
-          A grid system is a design tool used to arrange content on a webpage.
-          It is a series of vertical and horizontal lines that create a matrix
-          of intersecting points, which can be used to align and organize page
-          elements. Grid systems are used to create a consistent look and feel
-          across a website, and can help to make the layout more visually
-          appealing and easier to navigate.
-        </p>
-        <Image
-          src="/L009.jpg"
-          alt="blog post"
-          width={1024}
-          height={1024}
-          className="w-auto object-cover"
-        />
-        <p>
-          How do you create compelling presentations that wow your colleagues
-          and impress your managers?
-        </p>
-        <p>
-          A grid system is a design tool used to arrange content on a webpage.
-          It is a series of vertical and horizontal lines that create a matrix
-          of intersecting points, which can be used to align and organize page
-          elements. Grid systems are used to create a consistent look and feel
-          across a website, and can help to make the layout more visually
-          appealing and easier to navigate.
-        </p>
-        <p>
-          A grid system is a design tool used to arrange content on a webpage.
-          It is a series of vertical and horizontal lines that create a matrix
-          of intersecting points, which can be used to align and organize page
-          elements. Grid systems are used to create a consistent look and feel
-          across a website, and can help to make the layout more visually
-          appealing and easier to navigate.
-        </p>
+        <div dangerouslySetInnerHTML={{ __html: details?.content }} />
       </section>
     </main>
   );
